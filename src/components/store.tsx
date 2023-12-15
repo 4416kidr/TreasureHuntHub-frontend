b064f7fc695c8db505597000938db4da55cc493f
TOFU <4416kidr@gmail.com>
2023-12-16 04:06:07 +0900

    update store page

--- src/components/store.tsx
+++ src/components/store.tsx
@@ -6,11 +6,24 @@ type Props = {
     image: string;
   };
   
+  const stockTranslate = ( storeStock: number ) => {
+    switch (storeStock) {
+      case 0:
+        return 'nothing';
+      case 1:
+        return 'few';
+      case 2:
+        return 'full'
+      default:
+        return 
+    }
+  };
+
   const Store = ({ storeName, storeLocation, storeStock, comment, image}: Props) => (
     <div className="text-lime-500 place-content-center pl-2 py-2">
       <p>{storeName}</p>
       <p>{storeLocation}</p>
-      <p>在庫: {storeStock}</p>
+      <p>在庫: {stockTranslate(storeStock)}</p>
       <p>{comment}</p>
       <p>{image}</p>
     </div>
